3f8cb6d8e37948af4580815b3722263312991b69
Montaniz.Stills <montanizstills@gmail.com>
2023-09-17 16:14:11 -0400

    test(deploy-test): add debugging;

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -27,6 +27,8 @@ def basic_auth():
     auth_email = os.environ.get("JIRA_AUTH_EMAIL") if os.environ.get("JIRA_AUTH_EMAIL") else \
         json.load(open('../../resources/jira-credentials.json'))['jira_auth_email']
 
+    print(auth_email)
+
     token = os.environ.get("JIRA_API_KEY") if os.environ.get("JIRA_API_KEY") else \
         json.load(open('../../resources/jira-credentials.json'))['jira_api_key']
 
